929ac1bff55330952c98ad98e5e85b0f2052467f
L0STCOLE <93286109+L0STCOLE@users.noreply.github.com>
2024-04-03 21:26:20 +0000

    Change page.txs

--- my-bsky-app/src/lib/api.ts
+++ my-bsky-app/src/lib/api.ts
@@ -3,7 +3,7 @@ import { BskyAgent } from "@atproto/api";
 
 export const agent = new BskyAgent({
   // This is the AppView URL
-  service: "https://didconnect.tech",
+  service: "https://public.api.bsky.app",
   // If you were making an authenticated client, you would
   // use the PDS URL here instead - the main one is bsky.social
   // service: "https://bsky.social",
